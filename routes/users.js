0501b5fd6ba51ee0d2b3da581fe31673ab20d626
Sanju Shah <sanjushahgupta@gmail.com>
2024-03-29 09:23:09 +0100

    feat: register route - hash password

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 //@route    POST
 //@des      Register new user
@@ -17,13 +18,26 @@ router.post(
       "Please enter password with 5 or more characters."
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     var errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: errors.array() });
     }
+    const { name, password, email } = req.body;
 
-    res.send(req.body);
+    let user = await User.findOne({ email });
+    try {
+      if (user) {
+        return res.status(400).json({ msg: "Email already exists." });
+      }
+      const salt = await bcrypt.genSalt(10);
+      let hashedPassword = await bcrypt.hash(password, salt);
+      user = new User({ name, password: hashedPassword, email });
+      await user.save();
+      res.status(200).json({ msg: "User is registered." });
+    } catch (err) {
+      console.log(err.message);
+    }
   }
 );
 
