7d2ff362f2ca7da4cb572859a7a43d9f96f97942
Sanju Shah <sanjushahgupta@gmail.com>
2024-05-17 00:32:53 +0200

    chores:refactor + clean up

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,82 +1,98 @@
 const express = require("express");
 const router = express.Router();
-const contact = require("../models/Contacts");
+const contactModel = require("../models/Contacts");
 
+//@route get api/contacts
 //@desc get all contacts
 router.get("/", async (req, res) => {
   try {
-    await contact.find().then((data) => {
-      const listofContacts = data;
-      res.status(200);
-    });
+    const contacts = await contactModel.find();
+    res.status(200).json(contacts);
   } catch (e) {
-    res.status(501);
+    res
+      .status(500)
+      .json({ message: "An error occurred while fetching contacts" });
   }
 });
 
+//@route get api/contacts/:name
+//@desc get contact by name
 router.get("/:name", async (req, res) => {
   try {
-    await contact.find({ contactName: req.params.name }).then((data) => {
-      res.send(data).status(200);
-    });
+    const contact = await contactModel.find({ contactName: req.params.name });
+    if (contact.length === 0) {
+      return res.status(404).json({ message: "Contacts not found." });
+    }
+    res.status(200).json(contact);
   } catch (e) {
-    res.send("Sorry, Something went wrong.").status(500);
+    res
+      .status(500)
+      .json({ message: "An error occurred while fetching the contact" });
   }
 });
 
+//@route post api/contacts/
 //@desc add new contact
 router.post("/", async (req, res) => {
   try {
-    let contactName = req.body.contactName;
-    let telephone = req.body.telephone;
+    const { contactName, telephone } = req.body;
     if (!contactName || !telephone) {
-      res.status(400).send("Invalid inputs");
+      res.status(400).json({ message: "Invalid inputs" });
     }
-    await contact.create({
+    await contactModel.create({
       contactName: contactName,
       telephone: telephone,
     });
-    res.status(200).send("Contacts added successfully");
+    res.status(201).json({ message: "Contacts added successfully" });
   } catch (e) {
-    res.status(501);
+    res
+      .status(500)
+      .json({ message: "An error occurred while adding the contact" });
   }
 });
 
-//@route    PUT api/contacts/:name
-//@desc     update new contact details
+//@route PUT api/contacts/:name
+//@desc update new contact details
 
 router.put("/:name", async (req, res) => {
-  let nameExists = await contact.findOne({ contactName: req.params.name });
-  if (nameExists) {
-    await contact.updateOne(
+  try {
+    let existingContact = await contactModel.findOne({
+      contactName: req.params.name,
+    });
+    if (!existingContact) {
+      return res.status(400).json({ message: "Contact not found." });
+    }
+    await contactModel.updateOne(
       { contactName: req.params.name },
       {
         contactName: req.body.contactName,
         telephone: req.body.telephone,
       }
     );
-    res.send("contact updated").status(200);
-  } else {
-    res.send("contact not found.").status(500);
+    res.status(200).json({ message: "Contact updated sucessfully" });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "An error occurred while updating the contact" });
   }
 });
 
-//@route    Delete api/contacts/:id
-//@desc     delete contact
-//@access   Private
+//@route Delete api/contacts/:name
+//@desc Delete contact
 router.delete("/:name", async (req, res) => {
   try {
-    let contactsExists = await contact.findOne({
+    const existingContact = await contactModel.findOne({
       contactName: req.params.name,
     });
-    if (contactsExists) {
-      await contact.deleteOne({ contactName: req.params.name });
-      res.send("contact deleted");
-    } else {
-      res.send("Contact not found.");
+    if (!existingContact) {
+      return res.status(400).json({ message: "Contact not found" });
     }
+    await contactModel.deleteOne({ contactName: req.params.name });
+    res.status(200).json({ message: "Contact deleted sucessfully" });
   } catch (e) {
-    res.status(500).send(e);
+    res
+      .status(500)
+      .json({ message: "An error occurred while deleting the contact" });
   }
 });
 
